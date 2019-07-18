326497045cb7f623f9dbff7632231074154b7859
song <msms9957@gmail.com>
2019-07-19 00:01:36 +0900

    19/7/18

--- hello.py
+++ hello.py
@@ -42,7 +42,7 @@ print(a.rstrip())               #오른쪽 공백지우기
 a="i enjoy the eating"
 print(a.split())                #문자열 나누기 ()안에 들어가는것에 기준
 
-#리스트
+#리스트(값 변경o,[]로 감싼다)
 a = [1, 2, 3, ['a', 'b', 'c']]
 print(a[0])                     #1
 print(a[-1])                    #['a','b','c']
@@ -83,3 +83,42 @@ a.extend([1,4])                 #리스트확장
 print(a)
 a.sort()
 print(a)
+
+#튜플 (값변경x인 리스트,()로 감싼다)
+t1=(0,1,2,'a','b')
+print(t1[2])                    #인덱싱
+print(t1[2:])                   #슬라이싱
+
+#딕셔너리
+dic={'name':'harry','phone':'010','birth':'5/5'}
+c={1:'hi'}
+c={'c':[1,2,3]}                 #리스트도 삽입가능
+
+c={1:'a'}                       #첫 딕셔너리
+c[2]='b'                        #딕셔너리 쌍 추가
+c['name']='oracle'
+c[3]=[1,2,3]
+print (c)
+
+del c[1]                        #딕셔너리 삭제
+print(c)
+print(c['name'])                #key 사용해 value 얻기
+print(dic['name'])
+print(dic['phone'])
+
+c={1:'a',1:'b'}                 #key 중복 시
+print(c)                        #처음이 무시됨
+
+print(dic.keys())               #key 리스트
+f=list(dic.keys())              #dic.keys를 리스트로 변환하고자 할때
+print(f)
+print(dic.values())             #value 리스트
+print(dic.items())              #key,value 쌍얻기
+
+print(dic.get('name'))          #key로 value얻기
+print('name' in dic)            #해당키가 딕셔너리안에 있는지 조사
+#>>True
+print('email' in dic)
+#>>False
+dic.clear()                     #쌍 지우기
+print(dic)
